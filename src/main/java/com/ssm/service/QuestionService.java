a8a2cf2f70247fc6ac5f503477353316bad059d9
mwbx <81812825+mwbx@users.noreply.github.com>
2021-06-26 08:07:28 +0800

    deleteAll

--- src/main/java/com/ssm/service/QuestionService.java
+++
@@ -1,9 +0,0 @@
-package com.ssm.service;
-
-import com.ssm.pojo.ShortAnswerQuestions;
-
-import java.util.List;
-
-public interface QuestionService {
-    List<ShortAnswerQuestions> selectSAQ(int saqCourseId);
-}
